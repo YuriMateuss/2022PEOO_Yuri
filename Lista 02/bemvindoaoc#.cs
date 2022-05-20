f5dcbec88c542adf8140d86326efd0df8682a318
YuriLindo894 <105519122+YuriLindo894@users.noreply.github.com>
2022-05-20 09:23:41 -0300

    Create bemvindoaoc#.cs

---
+++ Lista 02/bemvindoaoc#.cs
@@ -0,0 +1,11 @@
+using System; 
+
+public class Program {
+
+  public static void Main(string[] args) {
+     Console.WriteLine("Digite seu nome completo: ");
+           string nome = Console.ReadLine();
+           string[] v = nome.Split();
+           Console.WriteLine("Bem Vindo ao C#, " + v[0]);
+  }
+}
