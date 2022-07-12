393b5c5dc1a07b1307584315f32ded49a62c69c8
Yuri Mateus <105519122+YuriLindo894@users.noreply.github.com>
2022-07-12 08:03:32 -0300

    Add files via upload

--- revisao03/Ex07.cs
+++ revisao03/Ex07.cs
@@ -3,6 +3,18 @@ using System;
 public class Program {
 
   public static void Main(string[] args) {
-    
-  }
+    Console.WriteLine("Digite uma frase:");
+    string s = Console.ReadLine();
+    string[] v = s.Split();
+    int n = 0;
+    while (n < v.Length) {
+      int i = n;
+      while(i < v.Length) {
+        Console.Write(v[i] + " ");
+        i++;
+      }
+      Console.WriteLine();
+      n++;
+    }
+  } 
 }
--- revisao03/Ex09.cs
+++ revisao03/Ex09.cs
@@ -3,6 +3,12 @@ using System;
 public class Program {
 
   public static void Main(string[] args) {
-    
+    Console.WriteLine("Digite uma frase:");
+    string palavra = Console.ReadLine();
+    int q = 0;
+    foreach (char p in palavra) {
+      q = q + 1;
+      Console.WriteLine($"{q} - {palavra}");
+    }
+    }
   }
-}
