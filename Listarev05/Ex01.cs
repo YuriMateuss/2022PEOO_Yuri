3e117d3fb4bcea19f3bce5637d1811bf66d0b431
Yuri Mateus <105519122+YuriLindo894@users.noreply.github.com>
2022-07-22 16:42:38 -0300

    Create Ex01.cs

---
+++ Listarev05/Ex01.cs
@@ -0,0 +1,17 @@
+using System; 
+
+public class Program {
+
+  public static void Main(string[] args) {
+    Console.WriteLine("Digite dois valores:");
+    double a = double.Parse(Console.ReadLine());
+    double b = double.Parse(Console.ReadLine());
+    double c = Menor(a, b);
+    Console.WriteLine(c);
+  }
+  public static double Menor(double x, double y) {
+    if (x < y) return x;
+    else return y;
+    
+  }
+}
