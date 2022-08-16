c23f22a544bc675b86dd6e21585e21812ee16c44
Yuri Mateus <105519122+YuriLindo894@users.noreply.github.com>
2022-08-16 07:59:09 -0300

    Create ex01.cs

---
+++ capC#/ex01.cs
@@ -0,0 +1,11 @@
+using System; 
+
+public class Program {
+
+  public static void Main(string[] args) {
+    Console.WriteLine("Informe seu nome: ");
+    string A = Console.ReadLine();
+    Console.WriteLine($"Olá, {A}, tudo bem?");
+    
+  }
+}
